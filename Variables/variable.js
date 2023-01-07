077336f18ee1298ce3361773b5607f698f1d2ac5
Jitesh Singh <Jiteshs101@gmail.com>
2023-01-07 14:56:49 +0530

    Javascript Variables

---
+++ Variables/variable.js
@@ -0,0 +1,14 @@
+var x = 10; // old way and have scoping problem
+let y = "Hello"; // Recommended
+const c = 100; // declare constant
+z = "world"; // allowed but bad practice
+var v; // declaration
+v = "50"; // rendering
+x = "h"; // allowed
+x = true; // allowed
+
+console.log(x);
+console.log(y);
+console.log(c);
+console.log(v);
+console.log(z);
