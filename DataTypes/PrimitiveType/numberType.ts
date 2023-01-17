74a1a3d6dd8b5da27e0a39c1e8e1c39bcf11629a
Jitesh Singh <Jiteshs101@gmail.com>
2023-01-17 10:08:51 +0530

    Number Datatype

---
+++ DataTypes/PrimitiveType/numberType.ts
@@ -0,0 +1,21 @@
+// keyword varName:datatype=value;
+
+let signed: number = +10;
+let unsigned: number = 10;
+let float: number = 10.211441; // 4 Byte
+let double: number = 8545213.565462; // 8 Byte
+let decimal: number = 4.47897974745456654; // Upto 29 digits after decimal
+let exponent: number = 2e3; //2000
+let binary: number = 0b101100;
+let octal: number = 0o56741;
+let hexadecimal: number = 0xf00d;
+
+console.log(signed);
+console.log(unsigned);
+console.log(float);
+console.log(double);
+console.log(decimal);
+console.log(exponent);
+console.log(binary);
+console.log(octal);
+console.log(hexadecimal);
