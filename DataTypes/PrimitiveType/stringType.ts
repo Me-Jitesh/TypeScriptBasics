076d3729f89061dd40c154c2088b6bd431b0d882
Jitesh Singh <Jiteshs101@gmail.com>
2023-01-21 19:29:09 +0530

    Back Tick Advantage

--- DataTypes/PrimitiveType/stringType.ts
+++ DataTypes/PrimitiveType/stringType.ts
@@ -3,3 +3,6 @@ let str2: string = "World !";
 let str3: string = `From Typescript`;
 
 console.log(str1 + " " + str2 + " " + str3);
+console.log(`${str1} ${str2} ${str3}`); //Simlify above expression
+// console.log'Hola From=${str3}');//Not Allowedd
+// console.log"Hola From=${str3}"); //not allowed
