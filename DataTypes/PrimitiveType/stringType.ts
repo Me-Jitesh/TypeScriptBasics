f2fa2efaa84b98920d2b924d8971b5a0ba1432e6
Jitesh Singh <Jiteshs101@gmail.com>
2023-01-22 21:29:08 +0530

    String Manipulation Functions

--- DataTypes/PrimitiveType/stringType.ts
+++ DataTypes/PrimitiveType/stringType.ts
@@ -6,3 +6,13 @@ console.log(str1 + " " + str2 + " " + str3);
 console.log(`${str1} ${str2} ${str3}`); //Simlify above expression
 // console.log'Hola From=${str3}');//Not Allowedd
 // console.log"Hola From=${str3}"); //not allowed
+
+console.log("************* String Manipulation Functions ************");
+
+let str: string = "0123456789 crAzy Typescript";
+
+console.log(str.charAt(5)); //5
+console.log(str.charCodeAt(1)); //49 ASCII Value
+console.log(str.indexOf("cr")); //11
+console.log(str.indexOf("cr7")); // -1 if not exist
+console.log(str.lastIndexOf("y")); //18, -1 for Capital Y
