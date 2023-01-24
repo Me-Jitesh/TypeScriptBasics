64fa9d0bcc312b75712b23a397c571900c545e13
Jitesh Singh <Jiteshs101@gmail.com>
2023-01-24 22:25:40 +0530

    More About SubString

--- DataTypes/PrimitiveType/stringType.ts
+++ DataTypes/PrimitiveType/stringType.ts
@@ -17,7 +17,7 @@ console.log(str.indexOf("cr")); //11
 console.log(str.indexOf("cr7")); // -1 if not exist
 console.log(str.lastIndexOf("y")); //18, -1 for Capital Y
 
-console.log("************* Substring ************");
+console.log("************* SubString ************");
 
 console.log(str.length);
 console.log(str.substring(0)); //0123456789 crAzy Typescript
@@ -25,7 +25,12 @@ console.log(str.substring(0, 9)); //012345678 , last index excluded
 console.log(str.substring(15, 7)); //789 crAz , it swaps the agument if start > end
 console.log(str.substring(0, 30)); // 0123456789 crAzy Typescript , replace end index with length
 console.log(str.substring(0, -25)); // print nothing  , replace end index with 0
-console.log(str.substring(0, NaN)); // print nothing  , replace end index with 0
+console.log(str.substring(5, -25)); // 01234  , replace end index with 0 and swap if start is greater
+console.log(str.substring(-25, 10)); // 0123456789  , replace -ve  index with 0
+console.log(str.substring(0, NaN)); // print nothing  , replace NaN index with 0
+console.log(str.substring(NaN, NaN)); // print nothing  , replace NaN index with 0
+console.log(str.substring(NaN, 10)); // 0123456789  , replace NaN index with 0
+console.log(str.substring(10, NaN)); // 0123456789  , replace NaN index with 0 and swap if start greater
 console.log(str.substring(9, 9)); // print nothing
 console.log(str.substring(28)); // print nothing
 console.log(str.substring(27, 30)); // print nothing
