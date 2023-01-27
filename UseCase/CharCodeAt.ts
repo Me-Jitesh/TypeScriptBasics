55e93ec85ca6a4894e0a4526e979733e50c6e646
Jitesh Singh <Jiteshs101@gmail.com>
2023-01-27 21:00:29 +0530

    Usecase charCodeAt Method

---
+++ UseCase/CharCodeAt.ts
@@ -0,0 +1,8 @@
+let fname: string = "jitesh";
+// let fname: string = "Jitesh";
+
+if (fname.charCodeAt(0) >= 65 && fname.charCodeAt(0) <= 90) {
+  console.log("Valid Format");
+} else {
+  console.log("invalid Format");
+}
