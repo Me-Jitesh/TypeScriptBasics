525e7c869eeb6a478c443aa487658966e7915c7c
Jitesh Singh <Jiteshs101@gmail.com>
2023-02-01 23:08:01 +0530

    null And undefined Type

---
+++ DataTypes/PrimitiveType/NullType.ts
@@ -0,0 +1,11 @@
+let var1;
+let var2: undefined;
+let var3 = undefined;
+let var4: null;
+let var5 = null;
+
+console.log(typeof var1); // undefined
+console.log(typeof var2); // undefined
+console.log(typeof var3); // undefined
+console.log(typeof var4); // undefined
+console.log(typeof var5); // object
